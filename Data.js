5808acc1af6fe984753ecee14a08827cf628cc30
MahithaBonthala <131877115+MahithaBonthala@users.noreply.github.com>
2023-08-23 12:48:26 +0530

    Add files via upload

---
+++ Data.js
@@ -0,0 +1,22 @@
+let allmusic = [
+{
+name:'Believer', 
+artist:'Imagine Dragons',
+img:'image1', 
+src:'music1'      
+ }, 
+{
+name:'Alone', 
+artist:'Alan Walker',
+img:'image2',
+src:'music2'
+},
+{
+name:'Legends never die', 
+artist:'Against the current',
+img:'image3',
+src:'music3'
+},
+]
+
+//You can add more songs like this
